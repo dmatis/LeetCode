f05cf4537745f807c2bf4e88289775183ae726bb
Darren Matis <darren.matis@gmail.com>
2015-09-09 08:30:25 -0700

    Added comments and error handling

--- src/SingleNumber.java
+++ src/SingleNumber.java
@@ -4,12 +4,26 @@
  * Needs to run in O(n) and without extra memory (if possible)
  **/
 
+import com.sun.javaws.exceptions.InvalidArgumentException;
+
 /**
  * Will return 0 as default if no single integer is in original array
+ * Program currently can only handle positive integers
  */
 public class SingleNumber {
 
-    public static int singleNumber(int[] nums) {
+    public static int singleNumber(int[] nums) throws IllegalArgumentException {
+        /** An array with only one unpaired element will always have an odd number of integers **/
+        if (nums.length % 2 == 0) {
+            throw new IllegalArgumentException("Must provide a array containing a single integer that only appears once");
+        }
+
+        /** Covers the condition that array is size 1 and simply returns that element **/
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        /** Current solution requires extra memory **/
         int[] tempArray = new int[nums.length];
 
         for (int i = 0; i < nums.length; i++) {
@@ -26,7 +40,9 @@ public class SingleNumber {
     public static void main(String[] args) {
         int[] array3 = {1,2,1,2,3,4,5,4,5};
         int[] array1 = {4,2,3,4,1,2,3};
-        System.out.println(singleNumber(array3));
-        System.out.println(singleNumber(array1));
+        int[] arrayOneElem = {1};
+        //System.out.println(singleNumber(array3));
+        //System.out.println(singleNumber(array1));
+        System.out.println(singleNumber(arrayOneElem));
     }
 }
