638cd2cde7ea34d70d908749e092e0266b4cf1c2
Darren Matis <darren.matis@gmail.com>
2015-09-10 09:56:02 -0700

    Resolved a bug for numbers that are multiple of 9

--- src/DigitalRoot.java
+++ src/DigitalRoot.java
@@ -4,12 +4,13 @@
 public class DigitalRoot {
 
     public static int computeDigitalRoot(int n) {
-        return n == 0 ? 0 : n == 9 ? 9 : n % 9;
+        return n == 0 ? 0 : n % 9 == 0 ? 9 : n % 9;
     }
 
     public static void main(String[] args) {
         System.out.println(computeDigitalRoot(38));
         System.out.println(computeDigitalRoot(9));
+        System.out.println(computeDigitalRoot(18));
         System.out.println(computeDigitalRoot(0));
         System.out.println(computeDigitalRoot(41));
     }
