7ef7c927563ffd236a046ca5614fc3c799c14989
dmatis <darren.matis@gmail.com>
2015-10-01 09:56:56 -0700

    Added additional comments on functionality

--- src/AnagramFinder.java
+++ src/AnagramFinder.java
@@ -2,7 +2,9 @@ import java.util.Arrays;
 
 public class AnagramFinder {
 
-// O(nlogn) solution
+/* O(nlogn) solution
+   Removes whitespaces, converts to char array, sorts and compares if equal
+ */
 public static boolean isAnagram(String s, String t) {
         char[] word1 = s.replaceAll("[\\s]", "").toCharArray();
         char[] word2 = t.replaceAll("[\\s]", "").toCharArray();
@@ -10,7 +12,7 @@ public static boolean isAnagram(String s, String t) {
         Arrays.sort(word2);
         return Arrays.equals(word1, word2);
     }
-
+    
     public static void main(String[] args) {
         String s1 = "hello";
         String t1 = "elloh";
