1e736931478506176af5f130998e58415ac57869
dmatis <darren.matis@gmail.com>
2015-10-01 09:39:49 -0700

    Algorithm for determining if string is anagram of the other

---
+++ src/AnagramFinder.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class AnagramFinder {
+
+// O(nlogn) solution
+public static boolean isAnagram(String s, String t) {
+        char[] word1 = s.replaceAll("[\\s]", "").toCharArray();
+        char[] word2 = t.replaceAll("[\\s]", "").toCharArray();
+        Arrays.sort(word1);
+        Arrays.sort(word2);
+        return Arrays.equals(word1, word2);
+    }
+
+    public static void main(String[] args) {
+        String s1 = "hello";
+        String t1 = "elloh";
+        String s2 = "arty";
+        String t2 = "party";
+        String s3 = "car";
+        String t3 = "rat";
+        String s4 = "aacc";
+        String t4 = "ccac";
+
+        System.out.println(isAnagram(s1,t1));
+        System.out.println(isAnagram(s2, t2));
+        System.out.println(isAnagram(s3,t3));
+        System.out.println(isAnagram(s4,t4));
+    }
+
+}
